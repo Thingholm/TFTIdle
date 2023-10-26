f6bdb459ba3cd02bf950490a89d5283f704c99d9
Rasmus Thingholm <rasmusthingholm@gmail.com>
2023-10-26 08:50:50 +0200

    disaheim copy

---
+++ Disaheim - Kopi (2)/Disaheim/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Controller
+    {
+        private List<Book> books;
+        private List<Amulet> amulets;
+
+        public List<Book> Books { get { return books; } }
+        public List<Amulet> Amulets { get { return amulets; } }
+
+        public Controller() 
+        {
+            books = new List<Book>();
+            amulets = new List<Amulet>();
+        }
+
+        public void AddToList(Book book)
+        {
+            books.Add(book);
+        }
+        public void AddToList(Amulet amulet)
+        {
+            amulets.Add(amulet);
+        }
+    }
+}
---
+++ Disaheim - Kopi (2)/Disaheim/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+            return book.Price;
+        }
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            double price;
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    price = 12.5;
+                    break;
+                case Level.medium:
+                    price = 20;
+                    break;
+                case Level.high:
+                    price = 27.5;
+                    break;
+                default:
+                    price = 0; 
+                    break;
+            }
+            return price;
+        }
+    }
+}
