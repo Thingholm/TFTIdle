9d344c580eb4620d65d92d5deffda104db38fe30
Thingholm <rasmusthingholm@gmail.com>
2023-11-01 10:30:41 +0100

    Pr19 2.1

--- Disaheim/Disaheim/Course.cs
+++ Disaheim/Disaheim/Course.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Disaheim
 {
-    public class Course
+    public class Course : IValuable
     {
+        private double courseHourValue = 875;
         private string name;
         public string Name { get { return name; } set { name = value; } }
         private int durationInMinutes;
         public int DurationInMinutes { get { return durationInMinutes; } set { durationInMinutes = value; } }
-
+        public double CourseHourValue { get { return courseHourValue; } set { courseHourValue = value; } }
         public Course(string name)
         { 
             this.name = name;
@@ -26,6 +27,13 @@ namespace Disaheim
         public override string ToString()
         {
             return $"Name: {name}, Duration in Minutes: {durationInMinutes}";
+        }
+        public double GetValue(Course course) 
+        {
+            int remainder = course.DurationInMinutes % 60;
+            int hours = course.DurationInMinutes / 60 + (remainder == 0 ? 0 : 1);
+            double price = hours * courseHourValue;
+            return price;
         }
     }
 }
--- Disaheim/Disaheim/Merchandise.cs
+++ Disaheim/Disaheim/Merchandise.cs
@@ -6,14 +6,53 @@ using System.Threading.Tasks;
 
 namespace Disaheim
 {
-    public abstract class Merchandise
+    public abstract class Merchandise : IValuable
     {
+        private double lowQualityValue = 12.5;
+        private double mediumQualityValue = 20;
+        private double highQualityValue = 27.5;
         private string itemId;
         public string ItemId { get; set; }
+        public double LowQualityValue { get { return lowQualityValue; } set { lowQualityValue = value; } }
+        public double MediumQualityValue { get { return mediumQualityValue; } set { mediumQualityValue = value; } }
+        public double HighQualityValue { get { return highQualityValue; } set { highQualityValue = value; } }
 
         public override string ToString()
         {
             return "ItemId: " + itemId;
+        }
+        public double GetValue(Merchandise merchandise)
+        {
+            if (merchandise is Amulet)
+            {
+                Amulet amulet = (Amulet)merchandise;
+                double price;
+                switch (amulet.Quality)
+                {
+                    case Level.low:
+                        price = lowQualityValue;
+                        break;
+                    case Level.medium:
+                        price = mediumQualityValue;
+                        break;
+                    case Level.high:
+                        price = highQualityValue;
+                        break;
+                    default:
+                        price = 0;
+                        break;
+                }
+                return price;
+            }
+            else if (merchandise is Book)
+            {
+                Book book = (Book)merchandise;
+                return book.Price;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
