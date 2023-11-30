f2625f9ad55a5df2234bea2af75ba52390e6503a
emilfynbo <113060208+emilfynbo@users.noreply.github.com>
2023-11-30 11:30:30 +0100

    ObserverPattern første ogpave + gitignore part 3

---
+++ Console App1/Console App1/ConcreteObserver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Console_App1
+{
+	public class ConcreteObserver : Observer
+	{
+        private ConcreteSubject subject;
+        public int State = 0;
+
+        public ConcreteObserver(ConcreteSubject subject)
+        {
+            this.subject = subject;
+        }
+
+        public override void Update()
+        {
+            State = subject.State;
+        }
+    }
+}
+
---
+++ Console App1/Console App1/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console_App1
+{
+	public abstract class Subject
+	{
+        private List<Observer> observers = new List<Observer>();
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+        public void Notify()
+        {
+            foreach (Observer o in observers)
+            {
+                o.Update();
+            }
+        }
+    }
+}
+
