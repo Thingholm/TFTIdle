c9e99661fde6eca91e8102fc0f076fae574f9625
emilfynbo <113060208+emilfynbo@users.noreply.github.com>
2023-11-30 11:27:06 +0100

    ObserverPattern første ogpave + gitignore part 2

---
+++ Console App1/Console App1/ConcreteSubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Console_App1
+{
+	public class ConcreteSubject : Subject
+	{
+        private int state = 0;
+        public int State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                Notify();
+            }
+        }
+    }
+}
+
