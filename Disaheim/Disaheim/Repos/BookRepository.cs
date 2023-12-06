6b81f6261f20ca5cc4cf477499e0d36bd91687cf
Thingholm <rasmusthingholm@gmail.com>
2023-12-06 09:40:52 +0100

    events proj

---
+++ Disaheim/Disaheim/Repos/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim.Repos
+{
+    public class BookRepository
+    {
+        private List<Book> books;
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+        public Book GetBook(string itemId)
+        {
+            return books.Find(x => x.ItemId == itemId);
+        }
+        public double GetTotalValue()
+        {
+            return books.Sum(x => );
+        }
+    }
+}
