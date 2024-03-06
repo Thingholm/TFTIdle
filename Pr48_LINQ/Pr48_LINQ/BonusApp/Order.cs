35bceb0f9cd6727a702888dcabf0497364e12357
Thingholm <rasmusthingholm@gmail.com>
2024-03-06 10:03:55 +0100

    pr48 finished

--- Pr48_LINQ/Pr48_LINQ/BonusApp/Order.cs
+++ Pr48_LINQ/Pr48_LINQ/BonusApp/Order.cs
@@ -18,12 +18,13 @@ namespace BonusApp
 
         public double GetValueOfProducts()
         {
-            double valueOfProducts = 0;
-            foreach (Product product in _products)
-            {
-                valueOfProducts += product.Value;
-            }
-            return valueOfProducts;
+            return _products.Sum(product => product.Value);
+        }
+
+        public double GetValueOfProducts(DateTime date)
+        {
+            return _products.Sum(product => (product.AvailableFrom <= date && product.AvailableTo >= date) ? product.Value : 0);
+            //return _products.Where(product => product.AvailableFrom <= date && product.AvailableTo >= date).Sum(product => product.Value);
         }
 
         public double GetBonus()
@@ -31,9 +32,39 @@ namespace BonusApp
             return Bonus(GetValueOfProducts());
         }
 
+        public double GetBonus (DateTime date)
+        {
+            return Bonus(GetValueOfProducts(date));
+        }
+
+        public double GetBonus (DateTime date, Func<double, double> BonusMethod)
+        {
+            return BonusMethod(GetValueOfProducts(date));
+        }
+
         public double GetTotalPrice()
         {
             return GetValueOfProducts() - GetBonus();
+        }
+
+        public double GetTotalPrice(DateTime date)
+        {
+            return GetValueOfProducts(date) - GetBonus(date);
+        }
+
+        public double GetTotalPrice(DateTime date, Func<double, double> BonusMethod) 
+        {
+            return GetValueOfProducts(date) - GetBonus(date, BonusMethod);
+        }
+
+        //public List<Product> SortProductOrderByAvailableTo()
+        //{
+        //    return _products.OrderBy(product => product.AvailableTo).ToList();
+        //}
+
+        public List<Product> SortProductOrderBy(Func<Product, object> keySelector)
+        {
+            return _products.OrderBy(keySelector).ToList();
         }
     }
 }
--- Pr48_LINQ/Pr48_LINQ/BonusAppUnitTest/UnitTest1.cs
+++ Pr48_LINQ/Pr48_LINQ/BonusAppUnitTest/UnitTest1.cs
@@ -15,17 +15,25 @@ namespace BonusAppUnitTest
             order.AddProduct(new Product
             {
                 Name = "Mælk",
-                Value = 10.0
+                Value = 10.0,
+                AvailableFrom = new DateTime(2018, 3, 1),
+                AvailableTo = new DateTime(2018, 3, 5)
             });
+
             order.AddProduct(new Product
             {
                 Name = "Smør",
-                Value = 15.0
+                Value = 15.0,
+                AvailableFrom = new DateTime(2018, 3, 3),
+                AvailableTo = new DateTime(2018, 3, 4)
             });
+
             order.AddProduct(new Product
             {
                 Name = "Pålæg",
-                Value = 20.0
+                Value = 20.0,
+                AvailableFrom = new DateTime(2018, 3, 4),
+                AvailableTo = new DateTime(2018, 3, 7)
             });
         }
         [TestMethod]
@@ -62,6 +70,34 @@ namespace BonusAppUnitTest
 
             order.Bonus = Bonuses.FlatTwoIfAmountMoreThanFive;
             Assert.AreEqual(43.0, order.GetTotalPrice());
+        }
+        [TestMethod]
+
+        public void GetValueOfProductsByDate_Test()
+        {
+            Assert.AreEqual(0.0, order.GetValueOfProducts(new DateTime(2018, 2, 28)));
+            Assert.AreEqual(10.0, order.GetValueOfProducts(new DateTime(2018, 3, 2)));
+            Assert.AreEqual(25.0, order.GetValueOfProducts(new DateTime(2018, 3, 3)));
+            Assert.AreEqual(45.0, order.GetValueOfProducts(new DateTime(2018, 3, 4)));
+        }
+
+        [TestMethod]
+        public void SortByAvailableToTest()
+        {
+            List<Product> result = order.SortProductOrderBy(x => x.Name);
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual("Smør", result[2].Name);
+            Assert.AreEqual("Mælk", result[0].Name);
+            Assert.AreEqual("Pålæg", result[1].Name);
+        }
+
+        [TestMethod]
+        public void GetTotalPriceByDate_Test()
+        {
+            Assert.AreEqual(0.0, order.GetTotalPrice(new DateTime(2018, 2, 28), (price) => price * 0.2));
+            Assert.AreEqual(8.0, order.GetTotalPrice(new DateTime(2018, 3, 2), (price) => price * 0.2));
+            Assert.AreEqual(20.0, order.GetTotalPrice(new DateTime(2018, 3, 3), (price) => price * 0.2));
+            Assert.AreEqual(36.0, order.GetTotalPrice(new DateTime(2018, 3, 4), (price) => price * 0.2));
         }
     }
 }
